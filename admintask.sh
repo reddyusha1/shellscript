eebe936fbb1b8c2645545b1dd3acbadf77b40bf0
reddyusha1 <ushagajjala2810@gmail.com>
2021-12-24 17:39:37 +0530

    daily admin tasks

---
+++ admintask.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#This Script is to display simple output 
+#purpose : To learn bash scripting
+#Creation Date : 24 Dec 2021
+#Modified Date : 24 Dec 2021
+#Author : Usha Gajjala
+echo
+top | head -10
+echo
+df -h
+echo
+free -m
+echo
+uptime
+echo
+iostat
+echo
