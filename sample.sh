bd1203941565bf4839cb70d58ae9ec8d026760b0
reddyusha1 <ushagajjala2810@gmail.com>
2021-12-24 17:37:29 +0530

    second commit

---
+++ sample.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This Script is to display simple output 
+#purpose : To learn bash scripting
+#Creation Date : 24 Dec 2021
+#Modified Date : 24 Dec 2021
+#Author : Usha Gajjala
+
+Note="This is Usha Reddy Gajjala"
+echo
+echo $Note
+echo
+date
+echo
+df -h
+
